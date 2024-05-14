e503cd9b335de69c00523e6f7db20c1eb51f03fe
Alexander Grattan <alex@grattan.me>
2024-05-13 21:15:04 -0400

    feat: working image texture in 3D with three.js

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,51 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import * as THREE from "three";
+import discusBag from "./images/discus-bag.png";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Scene Setup
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#FBC8FF");
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// Textures
+
+const texture = new THREE.TextureLoader().load(discusBag);
+texture.colorSpace = THREE.SRGBColorSpace;
+
+// Geometry
+
+const geometry = new THREE.PlaneGeometry(221, 282);
+const material = new THREE.MeshBasicMaterial({
+  map: texture,
+});
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
+
+camera.position.z = 500;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Orbit Controls
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Render
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+animate();
